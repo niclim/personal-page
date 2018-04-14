6a8d18873cafaa9b4fda3e99d6fcdd3f5a4428f3
Nicholas <18374483+niclim@users.noreply.github.com>
2018-04-14 14:57:39 -0700

    Added basic express server to serve app

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+const staticPath = path.join(__dirname, './build')
+
+// Middleware and setup
+app
+  .use(express.static(staticPath))
+
+app
+  .get('*', (req, res) => {
+    res.sendFile(staticPath)
+  })
+
+app.listen(PORT, () => {
+  console.log(`App is listening on PORT:${PORT}`)
+})
